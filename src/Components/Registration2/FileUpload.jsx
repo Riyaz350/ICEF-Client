68c71abc1c93e989802128dfce0ee44bf7334b84
riyaz <riyaz3582@gmail.com>
2024-09-23 00:13:22 +0600

    added code

---
+++ src/Components/Registration2/FileUpload.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useRef } from 'react';
+import { HiMiniXMark } from 'react-icons/hi2';
+import swal from "sweetalert";
+
+
+export default function FileUpload({     setFileData,  fileData }) {
+    console.log(fileData)
+  const fileInputRef = useRef();
+
+  // Handle file selection
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    
+    // Check if a file is selected and its size
+    if (selectedFile) {
+      if (selectedFile.size > 2 * 1024 * 1024) { // 2 MB limit
+        swal("File size must be less than 2 MB.");
+        return;
+      }
+      setFileData([selectedFile]); // Only allow one file
+    }
+  };
+
+  // Handle clear file
+  const handleClearFile = () => {
+    setFileData([]); // Clear the file
+  };
+
+  return (
+    <div className="file-upload">
+      {/* File Preview */}
+      {fileData?.length > 0 ? (
+        <div className="w-full mx-auto flex flex-col gap-2 rounded border-2 border-dashed border-primary-color p-3 bg-white">
+          <div className="flex items-center gap-x-6 rounded border-2 border-dashed border-primary-color p-3 bg-white">
+            <div className="flex-1 space-y-1.5 overflow-hidden">
+              <h5 className="text-sm font-medium tracking-tight truncate text-primary-color">
+                {fileData[0].name}
+              </h5>
+              <p className="text-gray-400 text-xs">
+                {(fileData[0].size / 1024).toFixed(1)} KB
+              </p>
+            </div>
+            <div
+              onClick={handleClearFile}
+              className="bg-primary-color p-1 rounded cursor-pointer"
+            >
+              <HiMiniXMark className="text-white" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        // Upload
+        <label
+          className="flex w-full flex-col items-center justify-center space-y-3 rounded border-2 border-dashed border-primary-color p-6 bg-white"
+          htmlFor="fileInput"
+        >
+          <svg
+            width={40}
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 42 32"
+            enableBackground="new 0 0 42 32"
+            xmlSpace="preserve"
+            fill="#7B7C00"
+          >
+            <path
+              fill="black"
+              d="M33.958,12.988C33.531,6.376,28.933,0,20.5,0C12.787,0,6.839,5.733,6.524,13.384 C2.304,14.697,0,19.213,0,22.5C0,27.561,4.206,32,9,32h6.5c0.276,0,0.5-0.224,0.5-0.5S15.776,31,15.5,31H9c-4.262,0-8-3.972-8-8.5 C1,19.449,3.674,14,9,14h1.5c0.276,0,0.5-0.224,0.5-0.5S10.776,13,10.5,13H9c-0.509,0-0.99,0.057-1.459,0.139 C7.933,7.149,12.486,1,20.5,1C29.088,1,33,7.739,33,14v1.5c0,0.276,0.224,0.5,0.5,0.5s0.5-0.224,0.5-0.5V14 c0-0.003,0-0.006,0-0.009c3.019,0.331,7,3.571,7,8.509c0,3.826-3.691,8.5-8,8.5h-7.5c-3.238,0-4.5-1.262-4.5-4.5V12.783l4.078,4.07 C25.176,16.951,25.304,17,25.432,17s0.256-0.049,0.354-0.146c0.195-0.195,0.195-0.513,0-0.707l-4.461-4.452 c-0.594-0.592-1.055-0.592-1.648,0l-4.461,4.452c-0.195,0.194-0.195,0.512,0,0.707s0.512,0.195,0.707,0L20,12.783V26.5 c0,3.804,1.696,5.5,5.5,5.5H33c4.847,0,9-5.224,9-9.5C42,17.333,37.777,13.292,33.958,12.988z"
+            ></path>
+          </svg>
+          <div className="space-y-1.5 text-center">
+            <h5 className="whitespace-nowrap text-sm font-medium tracking-tight text-primary-color">
+              Upload your File
+            </h5>
+            <p className="text-xs text-gray-400">
+              File formats: .pdf, .jpg, .jpeg, and .png 
+              <br />
+              Max size: 2 MB
+            </p>
+          </div>
+        </label>
+      )}
+
+      <input
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        className="hidden"
+        id="fileInput"
+        name='receipt'
+        type="file"
+         accept=".pdf, .jpg, .jpeg, .png"
+      />
+    </div>
+  );
+}
--- src/Components/Registration2/ThirdForm.jsx
+++ src/Components/Registration2/ThirdForm.jsx
@@ -6,25 +6,28 @@ import DocumentViewer from './Viewers/DocumentViewer';
 import useAxiosPublic from '../Hooks/useAxiosPublic';
 import Swal from 'sweetalert2';
 import { extractDateTime } from '../Tools/Time';
+import FileUpload from './FileUpload';
 
 const ThirdForm = ({ setPageNo, pageNo, setDocuments, firstFormData, secondFormData, errors, setErrors }) => {
   const [isDragging, setIsDragging] = useState(false);
-  const [fileData, setFileData] = useState([])
+  const [fileData, setFileData] = useState(null)
   const time = extractDateTime();
   const axiosPublic = useAxiosPublic()
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0]
-    if (file?.type?.startsWith('image/') || file?.type?.startsWith('application/pdf')) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const base64String = reader.result.split(',')[1];
-        setFileData(prevFiles => [...prevFiles, { string: base64String, name: file.name, type: file.type }]);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      alert('Please upload a pdf or image')
-    }
 
+  const handleFileUpload = (e) => {
+    // const file = e.target.files[0]
+    setFileData(e.target.files[0])
+    // if (file?.type?.startsWith('image/') || file?.type?.startsWith('application/pdf')) {
+    //   const reader = new FileReader();
+    //   reader.onloadend = () => {
+    //     const base64String = reader.result.split(',')[1];
+    //     setFileData(prevFiles => [...prevFiles, { string: base64String, name: file.name, type: file.type }]);
+    //   };
+    //   reader.readAsDataURL(file);
+    // } else {
+    //   alert('Please upload a pdf or image')
+    // }
+    console.log(fileData)
   };
 
   const handleDragEnter = (e) => {
@@ -53,8 +56,29 @@ const ThirdForm = ({ setPageNo, pageNo, setDocuments, firstFormData, secondFormD
     setErrors(prevErrors => [...prevErrors, e]);
   };
 
+
+
+
+
   const submitForm = (e) => {
     e.preventDefault()
+    setFileData(e.target.files[0])
+    console.log(e.target.files)
+    const formData = new FormData()
+    formData.append("firstName", firstFormData?.firstName);
+    formData.append("lastName", firstFormData?.lastName);
+    formData.append("email", firstFormData?.email);
+    formData.append("mobileNo", firstFormData?.mobileNo);
+    formData.append("whatsAppNo", firstFormData?.whatsAppNo);
+    formData.append("password", firstFormData?.password);
+    formData.append("companyDetails", secondFormData);
+    if (fileData) {
+      console.log(fileData)
+      formData.append("file", fileData); // Single file
+    }
+    for (const [key, value] of formData.entries()) {
+      console.log(`${key}: ${value}`);
+    }
     if (!firstFormData.firstName) addError(1);
     if (!firstFormData.lastName) addError(2);
     if (!firstFormData.email) addError(3);
@@ -74,12 +98,14 @@ const ThirdForm = ({ setPageNo, pageNo, setDocuments, firstFormData, secondFormD
     if (errors.length === 0 && firstFormData.firstName && firstFormData.lastName && firstFormData.email && firstFormData.mobileNo && firstFormData.password && firstFormData.password == firstFormData.rePassword
       && secondFormData.companyName && secondFormData.address && secondFormData.city && secondFormData.postCode && secondFormData.country && secondFormData.recruitCountry
     ) {
-      axiosPublic.post('/registrations', { personalInformation: firstFormData, companyDetails: secondFormData, document: fileData, time: time })
+
+      axiosPublic.post('/registrations', formData
+      )
         .then(res => {
           if (res.status === 200) {
             Swal.fire({ position: "top-end", icon: "success", title: "Success", text: `Thank You for submitting your form ${firstFormData?.firstName} ${firstFormData?.lastName}`, showConfirmButton: false, timer: 1500 });
             setTimeout(() => {
-              location.reload();
+              // location.reload();
             }, 2000);
           }
         })
@@ -92,12 +118,47 @@ const ThirdForm = ({ setPageNo, pageNo, setDocuments, firstFormData, secondFormD
   }, [fileData, setDocuments])
   return (
     <div>
-      <h1 className="text-xl font-semibold  text-gray-500 p-2 text-center">Please upload your portrait</h1>
+      {/* <h1 className="text-xl font-semibold  text-gray-500 p-2 text-center">Please upload your portrait</h1> */}
       <div>
-        <input type="file" name="upload" id="upload" placeholder="Drag and drop your pdf file here" onChange={handleFileUpload} onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDragOver={handleDragOver} onDrop={handleFileUpload}
-          className={`  rounded-lg w-2/3 bg-gray-100 mx-10 p-10 px-5 my-5  ${isDragging ? " border-purple-400 " : " border-black "}`} />
+        {/* <input
+         type="file" name="upload" id="upload" placeholder="Drag and drop your pdf file here" onChange={handleFileUpload} onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDragOver={handleDragOver} onDrop={handleFileUpload}
+          className={`  rounded-lg w-2/3 bg-gray-100 mx-10 p-10 px-5 my-5  ${isDragging ? " border-purple-400 " : " border-black "}`} /> */}
 
-        <div className='overflow-scroll   '>
+        <form
+          onSubmit={(e)=>submitForm(e)}
+          className="bg-white px-10 py-10 mt-4"
+          action="/upload" method="POST" encType="multipart/form-data"
+        >
+          <h1 className="md:text-lg text-base font-semibold capitalize mb-4 text-primary-color tracking-wider">
+            Share your Expense details
+          </h1>
+
+          {/* length checking label */}
+
+
+          <div className="rounded-md border border-gray-200">
+
+
+
+          </div>
+
+          <h1 className="md:text-lg text-base font-semibold capitalize my-4 text-primary-color tracking-wider">
+            Share your Expense Documents
+          </h1>
+
+          {/* FILE UPLOAD */}
+          <FileUpload setFileData={setFileData} fileData={fileData} />
+
+          {/* submit button */}
+          <div className="mt-5 flex justify-end">
+
+            <div type="submit" className="flex gap-20">
+              <button onClick={(e) => submitForm(e)} className="  w-full lg:ml-auto btn-primary text-base lg:text-lg text-white bg-[#7367f0] hover:bg-[#675dd8] rounded-xl p-2 my-10 font-bold">Submit</button>
+            </div>
+          </div>
+        </form>
+
+        {/* <div className='overflow-scroll   '>
           {fileData &&
             <div className='flex flex-col gap-2'>
               {fileData.map((data, index) =>
@@ -112,11 +173,9 @@ const ThirdForm = ({ setPageNo, pageNo, setDocuments, firstFormData, secondFormD
               )}
             </div>
           }
-        </div>
+        </div> */}
 
-        <div className="flex gap-20">
-          <button onClick={(e) => submitForm(e)} className="  w-full lg:ml-auto btn-primary text-base lg:text-lg text-white bg-[#7367f0] hover:bg-[#675dd8] rounded-xl p-2 my-10 font-bold">Submit</button>
-        </div>
+
 
       </div>
     </div>
