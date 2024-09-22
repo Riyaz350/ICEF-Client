7f7807dfd2f7147ec25bf165b83c7c441b08699f
sadmanryanrad <sadmanryanriad@gmail.com>
2024-09-23 02:38:07 +0600

    VITE

--- src/Components/Hooks/useAxiosPublic.jsx
+++ src/Components/Hooks/useAxiosPublic.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL:'https://icef-backend.vercel.app'
-    // baseURL:'http://localhost:5002'
-    baseURL:import.meta.env.URL
-})
+  // baseURL:'https://icef-backend.vercel.app'
+  // baseURL:'http://localhost:5002'
+  baseURL: import.meta.env.VITE_URL,
+});
 const useAxiosPublic = () => {
-    return axiosPublic
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
