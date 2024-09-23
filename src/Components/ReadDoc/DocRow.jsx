a06969ae0d7c311c1eea0537382998adaa833782
sadmanryanrad <sadmanryanriad@gmail.com>
2024-09-23 11:36:31 +0600

    download button added

--- src/Components/ReadDoc/DocRow.jsx
+++ src/Components/ReadDoc/DocRow.jsx
@@ -1,54 +1,104 @@
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { CiEdit } from "react-icons/ci";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../Authentication/AuthProvider";
 const DocRow = ({ registration }) => {
-    const thStyle = "font-normal text-base"
-    const { selectedRegistration, setSelectedRegistration } = useContext(AuthContext)
-    const handleSearch = () => {
-        setSelectedRegistration(registration)
-    }
-    const [photo, setPhoto] = useState()
-    // const handleDelete=()=>{
-    //     console.log('done')
-    // }
-    console.log(`${import.meta.env.VITE_IMG_URL}/${registration}`)
+  const thStyle = "font-normal text-base";
+  const { selectedRegistration, setSelectedRegistration } =
+    useContext(AuthContext);
+  const handleSearch = () => {
+    setSelectedRegistration(registration);
+  };
+  const [photo, setPhoto] = useState();
+  // const handleDelete=()=>{
+  //     console.log('done')
+  // }
+  console.log(`${import.meta.env.VITE_IMG_URL}/${registration.file.name}`);
 
-    return (
-        <tr className="border-b-[1px] font-thin border-gray-300 rounded-lg ">
-            <th className={thStyle}>{registration.firstName}{" "}{registration.lastName}</th>
-            <th className={thStyle}><p className="  text-start rounded-lg   font-semibold p-2">{registration?.mobileNo}</p></th>
-            <th className={thStyle}><p className="  text-start rounded-lg   font-semibold p-2">{registration?.email}</p></th>
-            <th className={thStyle}>{registration?.time}</th>
-            <th>
-                {/* Open the modal using document.getElementById('ID').showModal() method */}
-                <button className="btn" onClick={() => document.getElementById(registration._id).showModal()}>Show Details</button>
-                <dialog id={registration._id} className="modal modal-center sm:modal-middle">
-                    <div className="modal-box">
-                        <div>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Company Name:</span>{registration?.companyDetails.companyName}</p>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Address:</span>{registration?.companyDetails.address}</p>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Website:</span>{registration?.companyDetails.website}</p>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Post Code:</span>{registration?.companyDetails.postcode}</p>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Country:</span>{registration?.companyDetails.country}</p>
-                            <p className="py-4 font-normal"><span className="font-bold pr-2">Recruits from:</span>{registration?.companyDetails.recruitCountry}</p>
-                            <div>
-                                <img src={`${import.meta.env.VITE_IMG_URL}/${registration.file.name}`} alt="" />
-                            </div>
-                        </div>
-                        <div className="modal-action">
-                            <form method="dialog">
-                                {/* if there is a button in form, it will close the modal */}
-                                <button className="btn">Close</button>
-                            </form>
-                        </div>
-                    </div>
-                </dialog>
-            </th>
-
-        </tr>
-    );
+  return (
+    <tr className="border-b-[1px] font-thin border-gray-300 rounded-lg ">
+      <th className={thStyle}>
+        {registration.firstName} {registration.lastName}
+      </th>
+      <th className={thStyle}>
+        <p className="  text-start rounded-lg   font-semibold p-2">
+          {registration?.mobileNo}
+        </p>
+      </th>
+      <th className={thStyle}>
+        <p className="  text-start rounded-lg   font-semibold p-2">
+          {registration?.email}
+        </p>
+      </th>
+      <th className={thStyle}>{registration?.time}</th>
+      <th>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button
+          className="btn"
+          onClick={() => document.getElementById(registration._id).showModal()}
+        >
+          Show Details
+        </button>
+        <dialog
+          id={registration._id}
+          className="modal modal-center sm:modal-middle"
+        >
+          <div className="modal-box">
+            <div>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Company Name:</span>
+                {registration?.companyDetails.companyName}
+              </p>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Address:</span>
+                {registration?.companyDetails.address}
+              </p>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Website:</span>
+                {registration?.companyDetails.website}
+              </p>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Post Code:</span>
+                {registration?.companyDetails.postcode}
+              </p>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Country:</span>
+                {registration?.companyDetails.country}
+              </p>
+              <p className="py-4 font-normal">
+                <span className="font-bold pr-2">Recruits from:</span>
+                {registration?.companyDetails.recruitCountry}
+              </p>
+              <div>
+                <img
+                  src={`${import.meta.env.VITE_IMG_URL}/${
+                    registration.file.name
+                  }`}
+                  alt=""
+                />
+              </div>
+              {/* DOWNLOAD button  */}
+              <a
+                href={`${import.meta.env.VITE_IMG_DOWNLOAD_URL}/${
+                  registration.file.name
+                }`}
+                download
+              >
+                <button className="btn btn-success">Download</button>
+              </a>
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </th>
+    </tr>
+  );
 };
 
 export default DocRow;
--- src/Components/ReadDoc/ReadDoc.jsx
+++ src/Components/ReadDoc/ReadDoc.jsx
@@ -4,63 +4,60 @@ import ExportData from "./ExportData";
 import DocRow from "./DocRow";
 
 const ReadDoc = () => {
-    const [registrations, refetch] = useRegistrations()
-    console.log(registrations)
-    const regArray = [...registrations].reverse()
+  const [registrations, refetch] = useRegistrations();
+  console.log(registrations);
+  const regArray = [...registrations].reverse();
 
-    console.log(registrations)
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            refetch();
-        }, 1000);
+  console.log(registrations);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      refetch();
+    }, 1000);
 
-        return () => clearInterval(intervalId);
-    }, [refetch]);
+    return () => clearInterval(intervalId);
+  }, [refetch]);
 
+  return (
+    <div>
+      <div>
+        {registrations ? (
+          <div className=" overflow-scroll no-scrollbar bg-white max-w-5xl mx-auto shadow-lg  min-h-screen">
+            <div className="   bg-white w-full py-5 ">
+              <div>{/* <ExportData registrations={registrations} /> */}</div>
+              <table className="table w-full overflow-x-scroll">
+                <thead>
+                  <tr className="text-xl font-medium">
+                    <th>Counsellor NAME</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Time</th>
+                    <th>ACTION</th>
+                  </tr>
+                </thead>
 
-
-    return (
-        <div>
-
-            <div>
-                {registrations ? 
-                <div className=" overflow-scroll no-scrollbar bg-white max-w-5xl mx-auto shadow-lg  min-h-screen">
-
-                    < div className="   bg-white w-full py-5 " >
-                        <div>
-                            {/* <ExportData registrations={registrations} /> */}
-                        </div>
-                        <table className="table w-full overflow-x-scroll">
-                            <thead>
-                                <tr className="text-xl font-medium">
-                                    <th>Counsellor NAME</th>
-                                    <th>Phone</th>
-                                    <th>Email</th>
-                                    <th>Time</th>
-                                    <th>ACTION</th>
-                                </tr>
-                            </thead>
-
-                            {regArray ?
-                                <tbody className="">
-                                    {
-                                        regArray.map(registration => <DocRow key={registration._id} registration={registration} ></DocRow>)
-                                    }
-                                </tbody> :
-                                <span className="loading loading-spinner loading-lg"></span>
-                            }
-
-                        </table>
-                    </div >
-
-                </div > :
-                    <div className=" max-w-5xl mx-auto  min-h-screen  bg-white flex items-center justify-center">
-                    <span className=" loading loading-spinner loading-lg"></span>
-                </div>
-                    }
+                {regArray ? (
+                  <tbody className="">
+                    {regArray.map((registration) => (
+                      <DocRow
+                        key={registration._id}
+                        registration={registration}
+                      ></DocRow>
+                    ))}
+                  </tbody>
+                ) : (
+                  <span className="loading loading-spinner loading-lg"></span>
+                )}
+              </table>
             </div>
-        </div>
-    );
+          </div>
+        ) : (
+          <div className=" max-w-5xl mx-auto  min-h-screen  bg-white flex items-center justify-center">
+            <span className=" loading loading-spinner loading-lg"></span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ReadDoc;
